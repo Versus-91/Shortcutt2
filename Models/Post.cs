a6cc4d7e5acfe06f6547486564384bd04656d40c
ghader <gharibyan.ghader@gmail.com>
2021-11-25 23:31:52 +0330

    login styles change

---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Shortcutt.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public int MyProperty { get; set; }
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
